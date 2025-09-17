953d4190b22868575816c91ebd7b81692f38d8d5
Yash <yashurade27@gmail.com>
2025-09-17 22:12:13 +0530

    init components

---
+++ components/info/VisitUs.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VisitUs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VisitUs
---
+++ components/story/OurStory.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurStory = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OurStory
